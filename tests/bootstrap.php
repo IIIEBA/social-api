3c873215b33dafd97ec2e95a7090501f83db5503
Andrew Shevchuk <shevchuk@be-aware.com.ua>
2015-06-20 09:40:12 +0300

    Fix some code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,6 @@
 <?php
 
-// Composer autoloader
+// Composer autoload
 require __DIR__ .'/../vendor/autoload.php';
 
 error_reporting(E_ALL);
-
-// Tests autoloader
-spl_autoload_register(
-    function($className) {
-        if (strlen($className) > 6 && substr($className, 0, 6) === 'Tests\\') {
-            // This class should be loaded using bootstrap's autoloader
-            $trimmed = substr($className, 6);
-            $filename = __DIR__
-                . '/'
-                . str_replace('\\', '/', $trimmed)
-                . '.php';
-
-            if (file_exists($filename)) {
-                // Loading
-                include_once $filename;
-            }
-        }
-    }
-);
